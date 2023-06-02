1ea0a71f334b747579b9f2e99d8dc042cb42dbb4
Arla <98120709+ArlaZeqaj@users.noreply.github.com>
2023-06-02 23:18:16 +0200

    add encrypting feature

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 #include <string.h>
 #include <time.h>
 
+#define ALPHABET_SIZE 150
+#define MAX_LENGTH 300
+
+char alphabet[ALPHABET_SIZE];
+
+void createAlphabet(void)
+{
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        alphabet[i] = 'A' + i;
+    }
+}
+
 int isPrime(int n)
 {
     for (int i = 2; i <= sqrt(n); i++)
@@ -42,11 +55,67 @@ int public_key(int lambda_n)
     return -1;
 }
 
+int randome(int lambda_n)
+{
+    srand((unsigned int)time(NULL));
+    while (1)
+    {
+        int num = (rand() % (lambda_n - 5 + 1)) + 5;
+        if (isPrime(num) == 1)
+        {
+            return num;
+            break;
+        }
+    }
+    return -1;
+}
+
+long pomod(long a, long b, long m)
+{
+    long x = 1, y = a;
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+        {
+            x = (x * y) % m;
+        }
+        y = (y * y) % m;
+        b /= 2;
+    }
+    return x % m;
+}
+
+char* rsaEncrypt(char* message, long e, long n)
+{
+    createAlphabet();
+    long index;
+    long i;
+    long len = strlen(message);
+    char* cipher = (char*)malloc(len * sizeof(char));
+    for (i = 0; i < len; i++)
+    {
+        index = pomod(message[i], e, n);
+        cipher[i] = alphabet[index];
+    }
+    return cipher;
+}
+
 int main(void)
 {
     system("COLOR 02");
     int p = 7, q = 19, lambda_n, P, Q, lambda_N;
     long n, e, d, N;
+    char* message;
+    char* mgs;
+    char* text;
+    char* cipher;
+    char* sentence;
+    char* file_text;
+    char* encryptSentence;
+    char* encrySentence;
+    char choice[10];
+    char choice1[10];
+    createAlphabet();
     printf("\n\
          _______  _        _______  _______           _______ _________ _______  _______             \n\
         (  ____ \\( (    /|(  ____ \\(  ____ )|\\     /|(  ____ )\\__   __/(  ___  )(  ____ )  |\\     /| \n\
@@ -118,6 +187,21 @@ int main(void)
                 }
             }
         }
+        else if (number == 2)
+        {
+            n = p * q;
+            lambda_n = totient(p, q);
+            e = randome(lambda_n);
+            d = private_key(e, lambda_n);
+            printf("\n\tThe value of public key is %ld and modulus is %ld\n", d, n);
+            message = (char*)malloc(sizeof(char) * 100);
+            printf("\n\t~Enter the message: ");
+            scanf("%s", message);
+            cipher = rsaEncrypt(message, e, n);
+            printf("\n\t~The encrypted message is: %s\n", cipher);
+            free(message);
+            free(cipher);
+        }
         else
         {
             printf("\n\t~Invalid input. Please enter a number between 1 and 7.\n");
@@ -126,4 +210,3 @@ int main(void)
 
     return 0;
 }
-
